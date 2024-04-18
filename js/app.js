019a71467bd1b6163a8c450d29a12b54daa57b32
Juliane <juliane.trager@gmail.com>
2024-04-18 19:44:00 +0200

    Version 1.11 Slider + Media Queries

--- js/app.js
+++ js/app.js
@@ -29,10 +29,17 @@ window.addEventListener('scroll', handleScroll);
 
 const MainNavButton = document.getElementById('main-nav-btn');
 const MainNav = document.getElementById('main-nav');
+const Body = document.querySelector('body');
+const MainNavLinks = document.querySelectorAll('.main-nav a');
+
+for(let i = 0; i < MainNavLinks.length; i++){
+    MainNavLinks[i].addEventListener('click', toggleNav);
+}
 
 function toggleNav() {
-    MainNav.classList.toggle('hidden');
+    MainNav.classList.toggle('active');
     MainNavButton.classList.toggle('active');
+    Body.classList.toggle('no-scroll');
 }
 
 MainNavButton.addEventListener('click', toggleNav);
@@ -46,15 +53,72 @@ function toggleProdukt(e){
     if(e.target === ProduktButton_1){
         ProduktButton_1.classList.add('active');
         ProduktButton_2.classList.remove('active');
-        document.getElementById('TD_1ml').classList.toggle('hidden');
-        document.getElementById('TD_2ml').classList.toggle('hidden');
+        document.getElementById('TD_1ml').classList.remove('hidden');
+        document.getElementById('TD_2ml').classList.add('hidden');
     }else if(e.target === ProduktButton_2){
         ProduktButton_2.classList.add('active');
         ProduktButton_1.classList.remove('active');
-        document.getElementById('TD_1ml').classList.toggle('hidden');
-        document.getElementById('TD_2ml').classList.toggle('hidden');
+        document.getElementById('TD_1ml').classList.add('hidden');
+        document.getElementById('TD_2ml').classList.remove('hidden');
     }
 }
 
 ProduktButton_1.addEventListener('click', toggleProdukt);
 ProduktButton_2.addEventListener('click', toggleProdukt);
+
+// --------------------- Slider ---------------------
+
+
+const Slider = document.querySelectorAll('.slider');
+
+function changeSlide(e){
+    const sliderLength = e.target.scrollWidth; // Länge des Sliders
+    const sliderPosition = e.target.scrollLeft; // Scollposition des Sliders
+    const sliderElementCount = e.target.children.length; // Anzahl der Elemente im Slider
+    const x =  (sliderPosition / sliderLength) * sliderElementCount; // Berechnung des aktuellen Slides
+
+    for (let i = 0; i < sliderElementCount; i++){
+        if(i == x){
+            e.target.nextElementSibling.children[i].classList.add('active');
+        }else{
+            e.target.nextElementSibling.children[i].classList.remove('active');
+        }
+    }
+}
+
+for(let i = 0; i < Slider.length; i++){
+    Slider[i].addEventListener('scroll', changeSlide);
+}
+
+// Slider Arrows
+
+const sliderContainer = document.querySelectorAll('.slider-container');
+
+function slideLeft(e){
+    const slider = e.target.parentElement.parentElement.children[1];
+    const sliderWidth = slider.children[0].clientWidth;
+
+    slider.scrollLeft -= sliderWidth;
+}
+
+function slideRight(e){
+    const slider = e.target.parentElement.parentElement.children[1];
+    const sliderWidth = slider.children[0].clientWidth;
+
+    slider.scrollLeft += sliderWidth;
+}
+
+for(let i = 0; i < sliderContainer.length; i++){
+    // Linker Pfeil
+    sliderContainer[i].children[0].children[0].addEventListener('click', slideLeft);
+
+    if(sliderContainer[i].children[1].scrollLeft == 0){
+        sliderContainer[i].children[0].children[0].style.opacity = 0.3;
+    }
+    
+    // Rechter Pfeil
+    sliderContainer[i].children[0].children[1].addEventListener('click', slideRight);
+}
+
+
+
