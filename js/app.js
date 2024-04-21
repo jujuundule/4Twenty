16f1027ea2bb5ee8f329c3e3ca7c69c81092d4d8
Juliane Träger <juliane.trager@gmail.com>
2024-04-21 17:42:17 +0200

    Version 1.15 Weiterarbeit an Desktop Media Querie

--- js/app.js
+++ js/app.js
@@ -31,6 +31,7 @@ const MainNavButton = document.getElementById('main-nav-btn');
 const MainNav = document.getElementById('main-nav');
 const Body = document.querySelector('body');
 const MainNavLinks = document.querySelectorAll('.main-nav a');
+const header = document.querySelector('header');
 
 for(let i = 0; i < MainNavLinks.length; i++){
     MainNavLinks[i].addEventListener('click', toggleNav);
@@ -40,6 +41,7 @@ function toggleNav() {
     MainNav.classList.toggle('active');
     MainNavButton.classList.toggle('active');
     Body.classList.toggle('no-scroll');
+    header.classList.toggle('active');
 }
 
 MainNavButton.addEventListener('click', toggleNav);
